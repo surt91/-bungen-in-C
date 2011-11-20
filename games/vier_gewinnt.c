6354c41301c4bb341ec0453c368f7cf2c0c9b4eb
Hendrik Schawe <surt91@gmail.com>
2011-11-20 20:42:43 +0100

    Vier gewinnt lauffähig

--- games/vier_gewinnt.c
+++ games/vier_gewinnt.c
@@ -2,5 +2,179 @@
 
 void vier_gewinnt_start()
 {
-    printf("test\n");
+    int i, j, spieler = VG_KREIS;
+    char *spl;
+    struct vg_feld map;
+    map.runde = 0;
+    for(i=0;i<VG_X;i++)
+        for(j=0;j<VG_Y;j++)
+            map.status[i][j] = VG_LEER;
+
+    initscr();
+    raw();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+
+    while(1)
+    {
+        vg_draw(&map);
+        i = vg_steuern(&map);
+        vg_setzen(&map, i, spieler);
+
+        move(1,13);
+        if(vg_prufe_gewinn(&map))
+            break;
+
+        if(spieler == VG_KREIS)
+            spieler = VG_KREUZ;
+        else
+            spieler = VG_KREIS;
+
+    }
+    vg_draw(&map);
+    if(spieler == VG_KREIS)
+        spl = "Kreis";
+    else
+        spl = "Kreuz";
+    mvprintw(1,13,"%s gewinnt!", spl);
+    getch();
+    endwin();
+}
+
+void vg_draw(struct vg_feld *map)
+{
+    int i,j;
+
+    move(0,0);
+    //~ addch('+');
+    //~ for(i=0; i < VG_X; i++)
+        //~ addch('-');
+    //~ addch('+');
+    move(1,0);
+    for(j=0; j < VG_Y; j++)
+    {
+        printw("|");
+        for(i=0; i < VG_X; i++)
+        {
+            if(map->status[i][j] == VG_LEER)
+                addch('.');
+            else if(map->status[i][j] == VG_KREIS)
+                addch('O');
+            else if(map->status[i][j] == VG_KREUZ)
+                addch('X');
+            else
+                addch(' ');
+        }
+        addch('|');
+        move(j+2,0);
+    }
+    addch('+');
+    for(i=0; i < VG_X; i++)
+        addch('-');
+    addch('+');
+    return;
+}
+
+int vg_steuern(struct vg_feld *map)
+{
+    int k = '1';
+    int i = 0;
+    while(k != ' ')
+    {
+        move(0,i+1);
+        k = getch();
+        switch (k)
+        {
+            case 'a':
+            case 'A':
+            case KEY_LEFT:
+                if(!(i<=0))
+                    i--;
+                break;
+            case 'd':
+            case 'D':
+            case KEY_RIGHT:
+                if(!(i>=VG_X-1))
+                    i++;
+                break;
+            case ' ':
+            case 's':
+            case 'S':
+            case KEY_DOWN:
+                k = ' ';
+                if(map->status[i][0] != VG_LEER)
+                    k='x';
+                break;
+        }
+    }
+    return i;
+}
+
+void vg_setzen(struct vg_feld *map, int x, int spieler)
+{
+    int y=VG_Y-1;
+    while(map->status[x][y] != VG_LEER)
+        y--;
+    map->status[x][y] = spieler;
+}
+
+int vg_prufe_gewinn(struct vg_feld *map)
+{
+    int i,j;
+    for(i=0;i<VG_X;i++)
+        for(j=0;j<VG_Y;j++)
+            if(map->status[i][j] != VG_LEER)
+                if(vg_vier(map,i,j))
+                    return 1;
+    return 0;
+}
+
+int vg_vier(struct vg_feld *map, int x, int y)
+{
+    int winX = 1, winY = 1, winR = 1, winL = 1;
+    int now,j;
+    // Da von links -> rechts, oben -> unten gesucht wird, braucht man nur hier suchen:
+    // 1. nach rechts in gleicher x-Zeile
+    // 2. nach unten in gleicher y-Spalte
+    // 3. nach unten rechts in Diagonaler
+    // 3. nach unten links in Diagonaler
+    now = map->status[x][y];
+    for(j=1;j<4;j++)
+    {
+        // 1. Gleiche Zeile nach rechts
+        if(x<=VG_X-3-1)
+        {
+            if(now != map->status[x+j][y])
+                winX=0;
+        }
+        else
+            winX=0;
+        // 2. Gleiche Spalte nach unten
+        if(y<=VG_Y-3-1)
+        {
+            if(now != map->status[x][y+j])
+                winY=0;
+        }
+        else
+            winY=0;
+        // 3. Diagonale nach rechts unten
+        if(x<=VG_X-3-1 && y<=VG_Y-3-1)
+        {
+            if(now != map->status[x+j][y+j])
+                winR=0;
+        }
+        else
+            winR=0;
+        // 4. Diagonale nach unten links
+        if(x>=3 && y<=VG_Y-3-1)
+        {
+            if(now != map->status[x-j][y+j])
+                winL=0;
+        }
+        else
+            winL=0;
+    }
+
+    return winX+winY+winR+winL;
 }
--- games/vier_gewinnt.h
+++ games/vier_gewinnt.h
@@ -6,6 +6,24 @@
 
 #include <ncurses.h>
 
+#define VG_LEER  0
+#define VG_KREIS 1
+#define VG_KREUZ 2
+
+#define VG_X 7
+#define VG_Y 6
+
+struct vg_feld
+{
+    int status[VG_X][VG_Y];
+    int runde;
+};
+
 void vier_gewinnt_start();
+void vg_draw(struct vg_feld *map);
+int vg_steuern(struct vg_feld *map);
+void vg_setzen(struct vg_feld *map, int x, int spieler);
+int vg_prufe_gewinn(struct vg_feld *map);
+int vg_vier(struct vg_feld *map, int x, int y);
 
 #endif //VIER_GEWINNT_H
