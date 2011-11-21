c8dc4dd936bf61b38fa49ed6fb6111f5aba360b0
Hendrik Schawe <surt91@gmail.com>
2011-11-21 11:40:51 +0100

    Highlight der Siegersteine

--- games/vier_gewinnt.c
+++ games/vier_gewinnt.c
@@ -17,12 +17,13 @@ void vier_gewinnt_start()
     noecho();
     keypad(stdscr, TRUE);
 
+    vg_draw(&map);
     while(1)
     {
         mvprintw(1,13,"%s ist am Zug!", spl_str);
-        vg_draw(&map);
         i = vg_steuern(&map, spl_chr);
         vg_setzen(&map, i, spieler);
+        vg_draw(&map);
 
         move(1,13);
         if(vg_prufe_gewinn(&map))
@@ -41,7 +42,7 @@ void vier_gewinnt_start()
             spl_chr = 'O';
         }
     }
-    vg_draw(&map);
+
     mvprintw(1,13,"%s gewinnt!   ", spl_str);
     getch();
     endwin();
@@ -51,15 +52,10 @@ void vg_draw(struct vg_feld *map)
 {
     int i,j;
 
-    move(0,0);
-    //~ addch('+');
-    //~ for(i=0; i < VG_X; i++)
-        //~ addch('-');
-    //~ addch('+');
     move(1,0);
     for(j=0; j < VG_Y; j++)
     {
-        printw("|");
+        addch(ACS_VLINE);
         for(i=0; i < VG_X; i++)
         {
             if(map->status[i][j] == VG_LEER)
@@ -71,13 +67,15 @@ void vg_draw(struct vg_feld *map)
             else
                 addch(' ');
         }
-        addch('|');
+        addch(ACS_VLINE);
         move(j+2,0);
     }
-    addch('+');
+    addch(ACS_LLCORNER);
     for(i=0; i < VG_X; i++)
-        addch('-');
-    addch('+');
+        addch(ACS_HLINE);
+    addch(ACS_LRCORNER);
+    mvaddch(1,0,ACS_TTEE);
+    mvaddch(1,8,ACS_TTEE);
     return;
 }
 
@@ -129,19 +127,26 @@ void vg_setzen(struct vg_feld *map, int x, int spieler)
 
 int vg_prufe_gewinn(struct vg_feld *map)
 {
-    int i,j;
+    int i,j, win;
     for(i=0;i<VG_X;i++)
         for(j=0;j<VG_Y;j++)
             if(map->status[i][j] != VG_LEER)
-                if(vg_vier(map,i,j))
+            {
+                win = vg_vier(map, i, j);
+                if(win)
+                {
+                    vg_highlight(win, i, j);
                     return 1;
+                }
+            }
     return 0;
 }
 
 int vg_vier(struct vg_feld *map, int x, int y)
 {
-    int winX = 1, winY = 1, winR = 1, winL = 1;
+    int win = 0b1111;
     int now,j;
+
     // Da von links -> rechts, oben -> unten gesucht wird, braucht man nur hier suchen:
     // 1. nach rechts in gleicher x-Zeile
     // 2. nach unten in gleicher y-Spalte
@@ -154,35 +159,60 @@ int vg_vier(struct vg_feld *map, int x, int y)
         if(x<=VG_X-3-1)
         {
             if(now != map->status[x+j][y])
-                winX=0;
+                win = ~WINX & win;
         }
         else
-            winX=0;
+            win = ~WINX & win;
         // 2. Gleiche Spalte nach unten
         if(y<=VG_Y-3-1)
         {
             if(now != map->status[x][y+j])
-                winY=0;
+                win = ~WINY & win;
         }
         else
-            winY=0;
+            win = ~WINY & win;
         // 3. Diagonale nach rechts unten
         if(x<=VG_X-3-1 && y<=VG_Y-3-1)
         {
             if(now != map->status[x+j][y+j])
-                winR=0;
+                win = ~WINR & win;
         }
         else
-            winR=0;
+            win = ~WINR & win;
         // 4. Diagonale nach unten links
         if(x>=3 && y<=VG_Y-3-1)
         {
             if(now != map->status[x-j][y+j])
-                winL=0;
+                win = ~WINL & win;
         }
         else
-            winL=0;
+            win = ~WINL & win;
     }
+    return win;
+}
 
-    return winX+winY+winR+winL;
+void vg_highlight(int win, int x, int y)
+{
+    int j;
+    const static int modus = A_BOLD;
+    move(0,0);
+    clrtoeol();
+
+    //Nötig, da der Rand 1 dick ist
+    x++;
+    y++;
+
+    // Hebe die gewinnende Kombination hervor
+    if(win & WINX)
+        for(j=0;j<4;j++)
+            mvchgat(y, x+j, 1, modus, 0, NULL);
+    if(win & WINY)
+        for(j=0;j<4;j++)
+            mvchgat(y+j, x, 1, modus, 0, NULL);
+    if(win &WINR)
+        for(j=0;j<4;j++)
+            mvchgat(y+j, x+j, 1, modus, 0, NULL);
+    if(win & WINL)
+        for(j=0;j<4;j++)
+            mvchgat(y+j, x-j, 1, modus, 0, NULL);
 }
--- games/vier_gewinnt.h
+++ games/vier_gewinnt.h
@@ -13,6 +13,11 @@
 #define VG_X 7
 #define VG_Y 6
 
+#define WINX 0b0001
+#define WINY 0b0010
+#define WINR 0b0100
+#define WINL 0b1000
+
 struct vg_feld
 {
     int status[VG_X][VG_Y];
@@ -25,5 +30,6 @@ int vg_steuern(struct vg_feld *map, char sym);
 void vg_setzen(struct vg_feld *map, int x, int spieler);
 int vg_prufe_gewinn(struct vg_feld *map);
 int vg_vier(struct vg_feld *map, int x, int y);
+void vg_highlight(int win, int x, int y);
 
 #endif //VIER_GEWINNT_H
